17e5a37054c49766763ae711b6145f2d47861d7b
philgcc200011@fpt.edu.vn <98210418+LamPhiGCC200011@users.noreply.github.com>
2022-07-12 16:20:05 +0700

    search

--- controller/productController.js
+++ controller/productController.js
@@ -4,21 +4,43 @@ const Toy = mongoose.model('Toy');
 const router = express.Router();
 
 
-router.get('/doSearch', async(req, res) => {
-    let query = Toy.find()
-    if (req.query.Name == null && req.query.Name == '') {
-        query = query.regex('Name', new RegExp('req.query.Name', 'i'))
-    }
-    try {
-        const toys = await query.exec()
-        res.render('shops/index', {
-            toys: toys,
-            searchOptions: req.query
-        })
-    } catch {
-        res.render('/')
-    }
+// router.get('/doSearch', async(req, res) => {
+//     let query = Toy.find()
+//     if (req.query.Name == null && req.query.Name == '') {
+//         query = query.regex('Name', new RegExp('req.query.Name', 'i'))
+//     }
+//     try {
+//         const toys = await query.exec()
+//         res.render('shops/index', {
+//             toys: toys,
+//             searchOptions: req.query
+//         })
+//     } catch {
+//         res.render('/')
+//     }
 
+// })
+router.post('/doSearch', async(req, res) => {
+    let db = client.db('Toy');
+    let collection = db.collection('toys');
+    let name = new RegExp(req.body.search);
+
+    var condition = {
+        'Name': Name
+    };
+    var products = await collection.find(condition).toArray();
+
+    const template = handlebars.compile(fs.readFileSync('views/shops/index', 'utf-8'));
+    const results = template({
+        products3: products
+    }, {
+        allowProtoMethodsByDefault: false,
+        allowProtoPropertiesByDefault: false
+    })
+
+    res.render('/', {
+        content: results
+    })
 })
 
 
