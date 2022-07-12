0051f33a98ca8d31be5915899ca9d9b30443d4e3
philgcc200011@fpt.edu.vn <98210418+LamPhiGCC200011@users.noreply.github.com>
2022-07-12 16:44:02 +0700

    asd

--- controller/productController.js
+++ controller/productController.js
@@ -3,50 +3,6 @@ const mongoose = require('mongoose');
 const Toy = mongoose.model('Toy');
 const router = express.Router();
 
-
-// router.get('/doSearch', async(req, res) => {
-//     let query = Toy.find()
-//     if (req.query.Name == null && req.query.Name == '') {
-//         query = query.regex('Name', new RegExp('req.query.Name', 'i'))
-//     }
-//     try {
-//         const toys = await query.exec()
-//         res.render('shops/index', {
-//             toys: toys,
-//             searchOptions: req.query
-//         })
-//     } catch {
-//         res.render('/')
-//     }
-
-// })
-router.post('/doSearch', async(req, res) => {
-    let client = await MongoClient.connect(url, {
-        useUnifiedTopology: true
-    });
-    let db = client.db('Toy');
-    let collection = db.collection('toys');
-    let name = new RegExp(req.body.search);
-
-    var condition = {
-        'Name': Name
-    };
-    var products = await collection.find(condition).toArray();
-
-    const template = handlebars.compile(fs.readFileSync('views/shops/index', 'utf-8'));
-    const results = template({
-        products: products
-    }, {
-        allowProtoMethodsByDefault: false,
-        allowProtoPropertiesByDefault: false
-    })
-
-    res.render('/', {
-        content: results
-    })
-})
-
-
 router.get('/', (req, res) => {
     Toy.find((err, docs) => {
         if (!err) {
